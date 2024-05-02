be3a5e5caaa3ca85d105dec77182c1ac750d2443
mykhallaf <85395713+mykhallaf@users.noreply.github.com>
2024-05-02 19:07:43 +0300

    Add files via upload

---
+++ SocialMediaApp/gui/src/main/java/socialmediaproject/gui/LoginController.java
@@ -0,0 +1,90 @@
+package socialmediaproject.gui;
+
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
+import javafx.stage.Stage;
+import javafx.event.ActionEvent;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.stage.StageStyle;
+
+import java.io.File;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+
+
+public class LoginController implements Initializable{
+    @FXML
+    private Button cancelButton;
+    @FXML
+    private Button signupButton;
+    @FXML
+    private Label loginMessageLabel;
+    @FXML
+    private ImageView brandingImageView;
+    @FXML
+    private ImageView lockImageView;
+    @FXML
+    private TextField emailTextField;
+    @FXML
+    private PasswordField passwordField;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle){
+        File brandingFile = new File("Images/Asu connect.jpg");
+        Image brandingImage = new Image(brandingFile.toURI().toString());
+        brandingImageView.setImage(brandingImage);
+
+        File lockFile = new File("Images/lock.png");
+        Image lockImage = new Image(lockFile.toURI().toString());
+        lockImageView.setImage(lockImage);
+    }
+
+
+    public void loginButtonOnAction(ActionEvent event) {
+        String email = emailTextField.getText();
+        String password = passwordField.getText();
+
+        Login login = new Login(email, password);
+        if (login.authenticateUser()) {
+            loginMessageLabel.setText("Congratulations!");
+        } else {
+            loginMessageLabel.setText("Invalid input. Please try again.");
+        }
+    }
+
+    public void signupButtonOnAction(ActionEvent event) {
+       createAccountForm();
+    }
+
+    public void cancelButtonOnAction(ActionEvent event) {
+        Stage stage = (Stage) cancelButton.getScene().getWindow();
+        stage.close();
+    }
+
+
+    public void createAccountForm() {
+        try {
+            Stage stage = new Stage(); // Instantiate a new Stage object
+            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("Register.fxml"));
+            Parent root = fxmlLoader.load();
+            Scene scene = new Scene(root, 600, 600);
+            stage.initStyle(StageStyle.UNDECORATED);
+            stage.setScene(scene);
+            stage.show();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+
+}
